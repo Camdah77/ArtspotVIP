553c7802f457bcc5382ee3ad4e8a2823ebe69408
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-21 17:36:06 +0000

    updating url path

--- artproject/urls.py
+++ artproject/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from artspot.views import (landing_page, home, about)
+from artspot import views
+from artspot.views import landing_page, home, about
 from django.conf.urls.static import static
 
 app_name = 'artspot'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('artspot/', include('artspot.urls')), 
     path('about/', about, name='about'),
     path('', home, name='home')
 ]
--- artspot/views.py
+++ artspot/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
-
 # HTML- pages
 def landing_page(request):
     return render(request, 'base.html')
 def home(request):
     return render(request, 'index.html')  
+def about(request):
+    return render(request, 'about.about.html')  
